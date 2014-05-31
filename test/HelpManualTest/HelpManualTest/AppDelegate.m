dffc0a8220c49e87385c2023fe8cff5671c7ee03
Quentin Carnicelli <qdc@rogueamoeba.com>
2014-05-31 11:25:32 -0400

    Initial Commit

---
+++ test/HelpManualTest/HelpManualTest/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  HelpManualTest
+//
+//  Created by Quentin Carnicelli on 5/31/14.
+//  Copyright (c) 2014 Rogue Amoeba Software, Inc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ test/HelpManualTest/HelpManualTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HelpManualTest
+//
+//  Created by Quentin Carnicelli on 5/31/14.
+//  Copyright (c) 2014 Rogue Amoeba Software, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
